b1e456ff0c634db18ca238c75c402fba8a56794e
Victor Zuñiga <victorhugo.zuniga@outlook.com>
2023-07-13 21:14:09 -0600

    Carga perezosa en las imagenes

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  onLoad(): void {
+    this.hasLoaded = true;
+  }
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL property is required.');
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { LazyImageComponent } from './components/lazy-image/lazy-image.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 
 
 @NgModule({
   declarations: [
+    LazyImageComponent,
     SidebarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
+    LazyImageComponent,
     SidebarComponent
   ]
 })
